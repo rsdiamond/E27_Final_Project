34755ce5b20d6476d2c1150bbab89b9b2da3190e
rdiamon1 <rdiamon1@swarthmore.edu>
2016-05-03 16:14:59 -0400

    added config file for texton approach

---
+++ config.js
@@ -0,0 +1,17 @@
+{
+    "maxImageSize": 200,
+    "whitenImages": true,
+    "randomOffset": true,
+    "randomRotate": false,
+    "randomFlip": false,
+    "filterSize": 49,
+    "weberTransformResponses": true,
+    "weberScale": 0.03,
+    "kmeansAttempts": 10,
+    "kmeansMaxIter": 10,
+    "kmeansEps": 0.0,
+    "generateHTMLOutput": true,
+    "maxHTMLTrainImages": 3,
+    "maxHTMLTestImages": 20,
+    "baseDir": "images"
+}
